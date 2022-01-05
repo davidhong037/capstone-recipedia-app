cf5cbe4d2614ecffdd0fbbbdb43cb62d96db0fe5
davidhong037 <90651988+davidhong037@users.noreply.github.com>
2022-01-05 18:26:35 -0500

    update to server

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const path = require('path')
 const connection = require('./db')
 const userRoutes = require('./routes/users')
 const authRoutes = require('./routes/auth')
@@ -11,9 +12,17 @@ connection();
 app.use(express.json())
 app.use(cors())
 
+app.use(express.static((path.join(__dirname, 'client', 'build'))))
+
 app.use('/api/users', userRoutes)
 app.use('/api/auth', authRoutes)
 
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
+});
+
 const port = process.env.PORT || 4545
+
+console.log(process.env.NODE_ENV)
 
 app.listen(port, () => console.log(`Listening on port ${port}...`))
